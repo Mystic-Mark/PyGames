9066674847051777ab816254e21cad94b69b5822
Mystic-Mark <61856739+Mystic-Mark@users.noreply.github.com>
2020-03-07 08:10:54 -0500

    rename main

---
+++ marco_polo.py
@@ -0,0 +1,16 @@
+from common import (randomize, cls, center, waitToContinue)
+EP = list()
+JL, C, W, M, FP, BSK = 300, 2, 30, 5, 5, 99 #Inital quantities of stuff
+
+def initialize():
+    pass
+
+# Start Program
+randomize()
+cls()
+center("The Journey of Marco Polo, 1271\n")
+center("(c) David H. Ahl, 1986\n")
+center("Ported to Python by Mark Rapp, 2020")
+print('\n' * 2); waitToContinue()
+cls()
+
--- test.py
+++
@@ -1,9 +0,0 @@
-from common import (randomize, cls, center, waitToContinue)
-
-randomize()
-cls()
-center("The Journey of Marco Polo, 1271")
-center("(c) David H. Ahl, 1986")
-center("Ported to Python by Mark Rapp, 2020")
-print('\n' * 2); waitToContinue()
-cls()
